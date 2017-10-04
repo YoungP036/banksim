5bf9a9c497ad5cb7f9d306cf8cafd338ff45f4f6
Patrick Young <tua43500@temple.edu>
2017-10-04 13:13:21 -0400

    implemented 10 charge semaphore, works and is FAST!

--- src/edu/temple/cis/c3238/banksim/Bank.java
+++ src/edu/temple/cis/c3238/banksim/Bank.java
@@ -1,5 +1,10 @@
 package edu.temple.cis.c3238.banksim;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author Cay Horstmann
  * @author Modified by Paul Wolfgang
@@ -12,6 +17,10 @@ public class Bank {
     private final int initialBalance;
     private final int numAccounts;
     private boolean open;
+    public Lock accountLock;
+    public Condition testing;
+    public Semaphore transferingSem;
+    private int testCount;
 
     public Bank(int numAccounts, int initialBalance) {
         open = true;
@@ -22,26 +31,42 @@ public class Bank {
             accounts[i] = new Account(this, i, initialBalance);
         }
         ntransacts = 0;
+	accountLock=new ReentrantLock();
+	testing = accountLock.newCondition();
+	transferingSem=new Semaphore(10);
+	testCount=0;
     }
 
     public void transfer(int from, int to, int amount) {
         accounts[from].waitForAvailableFunds(amount);
         if (!open) return;
-        if (accounts[from].withdraw(amount)) {
-            accounts[to].deposit(amount);
-        }
+	try{
+	    transferingSem.acquire();
+	    if (accounts[from].withdraw(amount)) {
+		accounts[to].deposit(amount);
+	    }
+	}
+	catch(InterruptedException e){}
+	finally{transferingSem.release();}
         if (shouldTest()) test();
     }
 
     public void test() {
         int sum = 0;
+	
+	try{transferingSem.acquire(10);
+	////////////////CS START////////////////
         for (Account account : accounts) {
             System.out.printf("%s %s%n", 
                     Thread.currentThread().toString(), account.toString());
             sum += account.getBalance();
         }
-        System.out.println(Thread.currentThread().toString() + 
-                " Sum: " + sum);
+	////////////////CS END/////////////////
+	}catch(InterruptedException e){}
+	finally{transferingSem.release(10);}
+        
+	System.out.println(Thread.currentThread().toString() + 
+                "test " + testCount++ +" Sum: " + sum);
         if (sum != numAccounts * initialBalance) {
             System.out.println(Thread.currentThread().toString() + 
                     " Money was gained or lost");
